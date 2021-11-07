525ac075c75975116b20980df74708857cf6fae2
Rebeca Silva <rebeca.silva@liferay.com>
2021-11-07 13:35:28 -0300

    create form component

---
+++ src/components/Form.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Form = ({inputText, setInputText, todos, setTodos}) => {
+
+    const inputTextHandler = (event) => {
+        setInputText(event.target.value);
+    };
+    const submitTodoHandler = (event) => {
+        // this will prevent the page to refresh everytime we add a todo
+        event.preventDefault(); 
+        setTodos([
+            ...todos,
+            {
+                text: inputText,
+                completed: false,
+                id: Math.random() * 1000
+
+            }
+        ]);
+        setInputText("");
+    };
+    return (
+        <form>
+            <input 
+             value={inputText} 
+             onChange={inputTextHandler} 
+             className="todo-input" 
+             type="text" 
+            />
+            <button onClick={submitTodoHandler} className="todo-button" type="submit">
+                <i className="fas fa-plus-square"></i>
+            </button>
+            <div className="select">
+                <select name="todos" className="filter-todo">
+                    <option value="all">All</option>
+                    <option value="completed">Completed</option>
+                    <option value="uncompleted">Uncompleted</option>
+                </select>
+            </div>
+        </form>
+
+    );
+};
+
+export default Form;
